e6efb4eb2a2123712f871c20c907e081cce60b36
Anjin Lee <anjin720jin@naver.com>
2025-03-05 02:28:56 +0900

    first commit

---
+++ COMFIE/COMFIEApp.swift
@@ -0,0 +1,17 @@
+//
+//  COMFIEApp.swift
+//  COMFIE
+//
+//  Created by Anjin on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct COMFIEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COMFIETests/COMFIETests.swift
@@ -0,0 +1,17 @@
+//
+//  COMFIETests.swift
+//  COMFIETests
+//
+//  Created by Anjin on 3/5/25.
+//
+
+import Testing
+@testable import COMFIE
+
+struct COMFIETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
