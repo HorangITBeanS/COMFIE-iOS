c3c61492456069a762b3b4dda799643fdab1b20f
Anjin Lee <anjin720jin@naver.com>
2025-03-06 19:08:26 +0900

    Refactor: 앱 진입 시 rootView method 가독성을 위한 If문 변경

--- COMFIE/App/Router/Router.swift
+++ COMFIE/App/Router/Router.swift
@@ -14,7 +14,7 @@ class Router {
     var path: [Route] = .init()
     
     // 앱 실행 시, 2초간 로딩 화면 종료 여부
-    var isLoadingViewFinished: Bool = false  // 앱 최초 실행 시, 2초간 로딩 화면 종료 여부
+    var isLoadingViewFinished: Bool = false
     
     // 앱 최초 실행 여부 - 온보딩 화면 전환 확인
     var hasEverOnboarded: Bool = false {
@@ -24,7 +24,9 @@ class Router {
     }
     
     init() {
-        let result = UserDefaultsService().load(type: Bool.self, key: .hasEverOnboarded)
+        let userDefaults = UserDefaultsService()
+        let result = userDefaults.load(type: Bool.self, key: .hasEverOnboarded)
+        
         switch result {
         case .success(let data):
             hasEverOnboarded = data
@@ -36,16 +38,11 @@ class Router {
     // 가장 상위 View - 앱 진입 시 파악
     @ViewBuilder func rootView(_ router: Router) -> some View {
         if isLoadingViewFinished == false {
-            // 앱 진입 시, 로딩 View
-            Route.loading.view(router)
+            Route.loading.view(router)     // 앱 진입 시, 로딩 View
+        } else if hasEverOnboarded == false {
+            Route.onboarding.view(router)  // 앱 최초 실행 O -> 온보딩 View
         } else {
-            if hasEverOnboarded == false {
-                // 앱 최초 실행 O -> 온보딩 View
-                Route.onboarding.view(router)
-            } else {
-                // 앱 최초 실행 X -> 메모 View
-                Route.memo.view(router)
-            }
+            Route.memo.view(router)        // 앱 최초 실행 X -> 메모 View
         }
     }
 }
