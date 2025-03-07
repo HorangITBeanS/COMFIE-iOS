ac6049879039c9f8023ab5a858f218512f254c57
Anjin Lee <anjin720jin@naver.com>
2025-03-07 14:08:36 +0900

    Feat: Domain 레이어의 Entity 추가

---
+++ COMFIE/Domain/Entity/ComfieZone.swift
@@ -0,0 +1,14 @@
+//
+//  ComfieZone.swift
+//  COMFIE
+//
+//  Created by Anjin on 3/7/25.
+//
+
+import Foundation
+
+// 컴피존 위치 - 진짜 임시값이라서 변경 해줘
+class ComfieZone {
+    var location: String = ""
+    var customName: String?
+}
---
+++ COMFIE/Domain/Entity/Memo.swift
@@ -0,0 +1,16 @@
+//
+//  Memo.swift
+//  COMFIE
+//
+//  Created by Anjin on 3/7/25.
+//
+
+import Foundation
+
+// (이름은 생각 안하고 일단 쓰는거 ^^)
+struct Memo {
+    var date: Date              // 생성된 날짜
+    var text: String            // 원본 글자
+    var emojiText: String       // 이모지로 변환된 글자
+    var retrospection: String?  // 회고
+}
