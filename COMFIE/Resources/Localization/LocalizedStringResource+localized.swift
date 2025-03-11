4d94035b7618f79ca258a42f78679ce28387462f
Anjin Lee <anjin720jin@naver.com>
2025-03-12 01:40:17 +0900

    feat: StringLiterals 값을 LocalizedResources 타입으로 변경 후 대응

---
+++ COMFIE/Resources/Localization/LocalizedStringResource+localized.swift
@@ -0,0 +1,14 @@
+//
+//  LocalizedStringResource+localized.swift
+//  COMFIE
+//
+//  Created by Anjin on 3/12/25.
+//
+
+import Foundation
+
+extension LocalizedStringResource {
+    var localized: String {
+        return String(localized: self)
+    }
+}
