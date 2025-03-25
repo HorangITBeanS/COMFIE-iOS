33561e29b05f63127f12a09e97c13abcc887f905
zaehorang <jaho001@uos.ac.kr>
2025-03-25 13:28:45 +0900

    design: 메모 뷰의 네비게이션 바 및 하단 입력 필드 ui 구현

--- COMFIE/Presentation/Memo/MemoView.swift
+++ COMFIE/Presentation/Memo/MemoView.swift
@@ -2,28 +2,88 @@
 //  MemoView.swift
 //  COMFIE
 //
-//  Created by Anjin on 3/6/25.
+//  Created by zaehorang on 3/23/25.
 //
 
 import SwiftUI
 
 struct MemoView: View {
     @State var intent: MemoStore
+    @State private var newMemoText: String = ""
     
     var body: some View {
-        Text("메모 View")
-        
-        Button {
-            intent(.showRetrospectionView)
-        } label: {
-            Text("회고 화면으로")
+        ZStack {
+            Color.keyBackground.ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                navigationBarView
+                
+                ScrollView {
+                    
+                }
+                .overlay(alignment: .center) {
+                    Text("아직 메모가 없어요. \n다행이네요!")
+                }
+                .multilineTextAlignment(.center)
+                
+                memoInputView
+            }
         }
-        
-        Button {
-            intent(.showComfieZoneSettingView)
-        } label: {
-            Text("컴피존 설정 화면으로")
+    }
+    
+    var navigationBarView: some View {
+        HStack {
+            // 조건에 따라 변경하기
+            Image("icUncomfie")
+            // Image("icComfie")
+            
+            Spacer()
+            
+            Button {
+                // 페이지 이동
+            } label: {
+                Image("icLocation")
+                    .frame(width: 24, height: 24)
+            }
         }
+        .padding(.horizontal, 19)
+        .padding(.vertical, 16)
+        .background(.cfWhite)
+    }
+    
+    var memoInputView: some View {
+        HStack(alignment: .top, spacing: 12) {
+            TextField("입력", text: $newMemoText, axis: .vertical)
+                .lineLimit(1...4)
+                .padding(.vertical, 9)
+                .padding(.leading, 12)
+                .padding(.trailing, 8)
+                .background(Color.keyBackground)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+            
+            Button {
+                newMemoText = ""
+            } label: {
+                Image(systemName: "arrow.up")
+                    .foregroundStyle(.cfWhite)
+                    .frame(width: 24, height: 24)
+                    .padding(8)
+                    .background(.keyDeactivated)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+            }
+        }
+        .padding(16)
+        .background {
+            UnevenRoundedRectangle(
+                topLeadingRadius: 16,
+                topTrailingRadius: 16,
+                style: .continuous
+            )
+            .foregroundStyle(.cfWhite)
+            .shadow(color: .black.opacity(0.12), radius: 10, x: 0, y: -4)
+            .ignoresSafeArea()
+        }
+        .ignoresSafeArea(.keyboard, edges: .bottom)
     }
 }
 
