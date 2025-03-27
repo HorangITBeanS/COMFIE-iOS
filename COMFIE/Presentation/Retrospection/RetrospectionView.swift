380682356bbdffe15aba73e8a6b9c6363491287c
Guryss <lala1470@konkuk.ac.kr>
2025-03-27 14:50:29 +0900

    feat: 키보드 포커싱 기능 구현

--- COMFIE/Presentation/Retrospection/RetrospectionView.swift
+++ COMFIE/Presentation/Retrospection/RetrospectionView.swift
@@ -7,53 +7,71 @@
 
 import SwiftUI
 
+enum Field: Hashable {
+    case content
+}
+
 struct RetrospectionView: View {
     @State private var retrospectionContent: String = ""
+    @FocusState private var isKeyboardFocused: Bool
+    let memo: String = ""
     
     var body: some View {
         VStack(spacing: 0) {
-            // 임시 네비게이션바
+            // temporary navigation bar
             Rectangle()
                 .frame(height: 56)
             
             ZStack(alignment: .top) {
                 Color.keyBackground.ignoresSafeArea()
                 
-                VStack(spacing: 0) {
-                    HStack(spacing: 0) {
-                        Text(Date().toFormattedDateTimeString())
-                            .comfieFont(.systemBody)
-                            .foregroundStyle(.cfBlack.opacity(0.6))
+                ScrollView {
+                    VStack(spacing: 0) {
+                        HStack(spacing: 0) {
+                            Text(Date().toFormattedDateTimeString())
+                                .comfieFont(.systemBody)
+                                .foregroundStyle(.cfBlack.opacity(0.6))
+                            
+                            Spacer()
+                        }
+                        .padding(.bottom, 8)
+                        HStack(spacing: 0) {
+                            // memo.originalText
+                            Text(memo)
+                                .comfieFont(.body)
+                                .foregroundStyle(.textBlack)
+                            
+                            Spacer()
+                        }
                         
-                        Spacer()
-                    }
-                    .padding(.bottom, 8)
-                    HStack(spacing: 0) {
-                        // memo.originalText 연결
-                        Text("뭐라고 적어야 화가 나보일까?")
-                            .comfieFont(.body)
-                            .foregroundStyle(.textBlack)
+                        Rectangle()
+                            .frame(height: 1)
+                            .foregroundStyle(Color.cfGray)
+                            .padding(.vertical, 17)
                         
-                        Spacer()
-                    }
-                    
-                    Rectangle()
-                        .frame(height: 1)
-                        .foregroundStyle(Color.cfGray)
-                        .padding(.vertical, 17)
-                    
-                    TextField("",
-                              text: $retrospectionContent,
-                              axis: .vertical)
+                        TextField("",
+                                  text: $retrospectionContent,
+                                  axis: .vertical)
                         .comfieFont(.body)
                         .foregroundStyle(.textBlack)
                         .tint(.cfBlack)
-
+                        .focused($isKeyboardFocused)
+                    }
+                    .padding(24)
                 }
-                .padding(24)
             }
         }
         .navigationBarBackButtonHidden()
+        .onAppear {
+            if retrospectionContent.isEmpty {
+                DispatchQueue.main.asyncAfter(deadline: .now()) {
+                    isKeyboardFocused = true
+                }
+            }
+        }
+        .onTapGesture {
+            endTextEditing()
+        }
     }
 }
 
