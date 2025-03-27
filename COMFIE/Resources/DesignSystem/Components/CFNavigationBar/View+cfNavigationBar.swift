76cd9a9321ab1c55fec278d69fa13fb7e769522b
Anjin Lee <anjin720jin@naver.com>
2025-03-27 15:46:09 +0900

    feat: View에서 사용하기 위한 cfNavigationBar modifier 구현

---
+++ COMFIE/Resources/DesignSystem/Components/CFNavigationBar/View+cfNavigationBar.swift
@@ -0,0 +1,59 @@
+//
+//  View+cfNavigationBar.swift
+//  COMFIE
+//
+//  Created by Anjin on 3/27/25.
+//
+
+import SwiftUI
+
+struct CFNavigationBarModifier: ViewModifier {
+    let title: String
+    let isBackButtonHidden: Bool
+    let trailingButton: CFNavigationBarButton?
+    
+    func body(content: Content) -> some View {
+        VStack(spacing: 0) {
+            CFNavigationBar(
+                title: title,
+                isBackButtonHidden: isBackButtonHidden,
+                trailingButton: trailingButton
+            )
+            
+            content
+        }
+    }
+}
+
+extension View {
+    func cfNavigationBar(
+        _ title: String,
+        isBackButtonHidden: Bool = false,
+        trailingButton: CFNavigationBarButton? = nil
+    ) -> some View {
+        modifier(
+            CFNavigationBarModifier(
+                title: title,
+                isBackButtonHidden: isBackButtonHidden,
+                trailingButton: trailingButton
+            )
+        )
+    }
+}
+
+#Preview {
+    VStack {
+        Text("hello")
+        Spacer()
+        HStack { Spacer() }
+    }
+    .background(Color.red.opacity(0.4))
+    .cfNavigationBar(
+        "title",
+        isBackButtonHidden: true,
+        trailingButton: CFNavigationBarButton(
+            image: Image(.icInfo),
+            action: {}
+        )
+    )
+}
