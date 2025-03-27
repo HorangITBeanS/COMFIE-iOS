41180f140a0f94f93cb4289c6859b4fa3c640c39
Anjin Lee <anjin720jin@naver.com>
2025-03-27 15:45:16 +0900

    feat: 뒤로가기 제스처 활성화

---
+++ COMFIE/Resources/Extensions/UINavigationController+popGesture.swift
@@ -0,0 +1,19 @@
+//
+//  UINavigationController+popGesture.swift
+//  COMFIE
+//
+//  Created by Anjin on 3/27/25.
+//
+
+import UIKit
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
