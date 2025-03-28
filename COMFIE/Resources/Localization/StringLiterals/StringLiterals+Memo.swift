7a20c5ae099f0dad9013776300465544e0a741a9
zaehorang <jaho001@uos.ac.kr>
2025-03-28 16:05:44 +0900

    add: Memo 뷰에서 사용되는 문자열 리터럴 추가

---
+++ COMFIE/Resources/Localization/StringLiterals/StringLiterals+Memo.swift
@@ -0,0 +1,15 @@
+//
+//  StringLiterals+Memo.swift
+//  COMFIE
+//
+//  Created by zaehorang on 3/25/25.
+//
+
+import Foundation
+
+extension StringLiterals {
+    enum Memo {
+        static let emptyMemoDescription: LocalizedStringResource = "memo_empty_memo"
+        static let textfieldPlaceholder: LocalizedStringResource = "memo_textfield_placeholder"
+    }
+}
