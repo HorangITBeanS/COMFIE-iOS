15e5cf0f83a9bbe804fe960b4db04cf28d791980
Anjin Lee <anjin720jin@naver.com>
2025-04-01 23:46:51 +0900

    feat: 네비게이션 버튼 변경사항(View 커스텀, Action 추가, 여러개) 반영

--- COMFIE/Resources/DesignSystem/Components/CFNavigationBar/CFNavigationBar.swift
+++ COMFIE/Resources/DesignSystem/Components/CFNavigationBar/CFNavigationBar.swift
@@ -9,14 +9,24 @@ import SwiftUI
 
 struct CFNavigationBarButton: Identifiable {
     let id: UUID = UUID()
-    let image: Image
     let action: () -> Void
+    let label: AnyView
+    
+    init<Content: View>(
+        action: @escaping () -> Void,
+        @ViewBuilder label: () -> Content
+    ) {
+        self.action = action
+        self.label = AnyView(label())
+    }
 }
 
 struct CFNavigationBar: View {
     let title: String
     let isBackButtonHidden: Bool
-    let trailingButton: CFNavigationBarButton?
+    let backButtonAction: () -> Void
+    let leadingButtons: [CFNavigationBarButton]
+    let trailingButtons: [CFNavigationBarButton]
     
     @Environment(\.dismiss) private var dismiss
     
@@ -28,32 +38,66 @@ struct CFNavigationBar: View {
                 .foregroundStyle(Color.textBlack)
             
             HStack(spacing: 0) {
-                // 왼쪽 뒤로가기 버튼
-                if isBackButtonHidden == false {
-                    Button {
-                        dismiss()
-                    } label: {
-                        Image(.icBack)
-                            .resizable()
-                            .frame(width: 24, height: 24)
+                // 왼쪽 버튼들
+                HStack(spacing: 8) {
+                    if isBackButtonHidden == false {
+                        backButton()  // 뒤로가기 버튼
                     }
+                    
+                    navigationButtons(for: leadingButtons)
                 }
                 
                 Spacer()
                 
-                // 오른쪽 버튼
-                if let button = trailingButton {
-                    Button {
-                        button.action()
-                    } label: {
-                        button.image
-                            .resizable()
-                            .frame(width: 24, height: 24)
-                    }
+                // 오른쪽 버튼들
+                HStack(spacing: 8) {
+                    navigationButtons(for: trailingButtons)
                 }
             }
             .padding(.horizontal, 20)
         }
         .frame(height: 56)
+        .background(Color.cfWhite)
     }
+    
+    @ViewBuilder
+    private func backButton() -> some View {
+        Button {
+            backButtonAction()
+            dismiss()
+        } label: {
+            Image(.icBack)
+                .resizable()
+                .frame(width: 24, height: 24)
+        }
+    }
+    
+    @ViewBuilder
+    private func navigationButtons(for buttons: [CFNavigationBarButton]) -> some View {
+        ForEach(buttons) { button in
+            Button {
+                button.action()
+            } label: {
+                button.label
+            }
+        }
+    }
+}
+
+#Preview {
+    VStack {
+        Text("hello")
+    }
+    .cfNavigationBar(
+        "title",
+        trailingButtons: [
+            CFNavigationBarButton {
+                // action
+            } label: {
+                Image(.icInfo)
+                    .resizable()
+                    .frame(width: 24, height: 24)
+            }
+        ]
+    )
 }
