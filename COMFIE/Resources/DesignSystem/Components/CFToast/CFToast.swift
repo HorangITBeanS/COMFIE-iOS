35d54397ac1f6c065fc12eb409523bb5c7dfca2a
Guryss <lala1470@konkuk.ac.kr>
2025-06-30 23:06:35 +0900

    fix: CFToast 뷰 구조 내 불필요한 ZStack 제거

--- COMFIE/Resources/DesignSystem/Components/CFToast/CFToast.swift
+++ COMFIE/Resources/DesignSystem/Components/CFToast/CFToast.swift
@@ -13,21 +13,18 @@ struct CFToast: View {
     let content: String
     
     var body: some View {
-        ZStack(alignment: .bottom) {
-            VStack(spacing: 0) {
-                Spacer()
-                HStack(spacing: 0) {
-                    Text(content)
-                        .comfieFont(.body)
-                        .foregroundStyle(textColor)
-                        .padding(.horizontal, 16)
-                        .padding(.vertical, 10)
-                }
-                .background(backgroundColor)
-                .clipShape(RoundedRectangle(cornerRadius: 8))
-                .padding(.bottom, 90)
+        VStack(spacing: 0) {
+            Spacer()
+            HStack(spacing: 0) {
+                Text(content)
+                    .comfieFont(.body)
+                    .foregroundStyle(textColor)
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 10)
             }
+            .background(backgroundColor)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .padding(.bottom, 90)
         }
-        .ignoresSafeArea(edges: .bottom)
     }
 }
